873ecee2bafb258ee177e09c35dcbea10989a6e6
Loïs Berndsen <L.Berndsen@student.han.nl>
2019-11-04 15:11:46 +0100

    can't open same dialog twice

--- dialog.js
+++ dialog.js
@@ -1,21 +1,15 @@
-// Dit is puur voor onszelf om bij te kunnen houden wat de positie is (met verticale scroll)
-// $(window).scroll(function() {
-//     const currentHeight = $(window).scrollTop();
-//     console.log("Vertical: " + currentHeight);
-// });
-//
-// // Zelfde maar dan voor horizontale scroll
-// $(window).scroll(function() {
-//     const currentHeight = $(window).scrollLeft();
-//     console.log("Horizontal: " + currentHeight);
-// });
-
 // Deze moeten nog aangepast worden o.b.v. functie hierboven (handmatig)
 const scrollPositionDialog1 = 200;
 const scrollPositionDialog2 = 400;
 const scrollPositionDialog3 = 600;
 const scrollPositionDialog4 = 800;
 const scrollPositionDialog5 = 1000;
+
+let dialog1HasBeenOpened = false;
+let dialog2HasBeenOpened = false;
+let dialog3HasBeenOpened = false;
+let dialog4HasBeenOpened = false;
+let dialog5HasBeenOpened = false;
 
 function disableBodyScrolling() {
     $('#disableScrollDiv').css({
@@ -40,7 +34,7 @@ function enableBodyScrolling() {
 
 function checkScrollPosition(heightOrWidth) {
     console.log(heightOrWidth);
-    if (heightOrWidth > scrollPositionDialog1 - 10 && heightOrWidth < scrollPositionDialog1 + 10) {
+    if (heightOrWidth > scrollPositionDialog1 - 10 && heightOrWidth < scrollPositionDialog1 + 10 && dialog1HasBeenOpened === false) {
         $("#dialog1").dialog({
             buttons: [
                 {
@@ -63,127 +57,127 @@ function checkScrollPosition(heightOrWidth) {
             open: disableBodyScrolling,
             close: enableBodyScrolling
         }).dialog("widget").find(".ui-dialog-title").hide();
-    } else if (heightOrWidth > scrollPositionDialog2 - 10 && heightOrWidth < scrollPositionDialog2 + 10) {
-            $("#dialog2").dialog({
-                buttons: [
-                    {
-                        text: "Take the risk",
-                        click: function () { // Er wordt geklikt/keuze gemaakt
-                            console.log("Take the risk");
-                            $(this).dialog("close"); // Pop up gaat weg
-                            // grabSword();
-                        }
-                    },
-                    {
-                        text: "I'll skip",
-                        click: function () { // Er wordt geklikt/keuze gemaakt
-                            console.log("I'll skip");
-                            $(this).dialog("close"); // Pop up gaat weg
-                            // skipSword();
-                        }
-                    },
-                ],
-                open: disableBodyScrolling,
-                close: enableBodyScrolling
-            }).dialog("widget").find(".ui-dialog-title").hide();
-    } else if (heightOrWidth > scrollPositionDialog3 - 10 && heightOrWidth < scrollPositionDialog3 + 10) {
-            $("#dialog3").dialog({
-                buttons: [
-                    {
-                        text: "Eat it!",
-                        click: function () { // Er wordt geklikt/keuze gemaakt
-                            console.log("Eat it!");
-                            $(this).dialog("close"); // Pop up gaat weg
-                            // eatMushroom();
-                        }
-                    },
-                    {
-                        text: "I hate mushrooms",
-                        click: function () { // Er wordt geklikt/keuze gemaakt
-                            console.log("I hate mushrooms");
-                            $(this).dialog("close"); // Pop up gaat weg
-                            // noEatMushroom();
-                        }
-                    },
-                ],
-                open: disableBodyScrolling,
-                close: enableBodyScrolling
-            }).dialog("widget").find(".ui-dialog-title").hide();
-    } else if (heightOrWidth > scrollPositionDialog4 - 10 && heightOrWidth < scrollPositionDialog4 + 10) {
-            $("#dialog4").dialog({
-                buttons: [
-                    {
-                        text: "Use sword",
-                        click: function () { // Er wordt geklikt/keuze gemaakt
-                            console.log("Use sword");
-                            $(this).dialog("close"); // Pop up gaat weg
-                            // attackSword();
-                        }
-                    },
-                    {
-                        text: "Slap him",
-                        click: function () { // Er wordt geklikt/keuze gemaakt
-                            console.log("Slap him");
-                            $(this).dialog("close"); // Pop up gaat weg
-                            // attackSlap();
-                        }
-                    },
-                    {
-                        text: "Scream at him",
-                        click: function () { // Er wordt geklikt/keuze gemaakt
-                            console.log("Scream at him");
-                            $(this).dialog("close"); // Pop up gaat weg
-                            // attackScream();
-                        }
-                    },
-                ],
-                open: disableBodyScrolling,
-                close: enableBodyScrolling
-            }).dialog("widget").find(".ui-dialog-title").hide();
-    } else if (heightOrWidth > scrollPositionDialog5 - 10 && heightOrWidth < scrollPositionDialog5 + 10) {
-            $("#dialog5").dialog({
-                buttons: [
-                    {
-                        text: "Return, duh",
-                        click: function () { // Er wordt geklikt/keuze gemaakt
-                            console.log("Return, duh");
-                            $(this).dialog("close"); // Pop up gaat weg
-                            // returnGold();
-                        }
-                    },
-                    {
-                        text: "I could use it...",
-                        click: function () { // Er wordt geklikt/keuze gemaakt
-                            console.log("I could use it...");
-                            $(this).dialog("close"); // Pop up gaat weg
-                            // keepGold();
-                        }
-                    },
-                ],
-                open: disableBodyScrolling,
-                close: enableBodyScrolling
-            }).dialog("widget").find(".ui-dialog-title").hide();
+
+        dialog1HasBeenOpened = true;
+    } else if (heightOrWidth > scrollPositionDialog2 - 10 && heightOrWidth < scrollPositionDialog2 + 10 && dialog2HasBeenOpened === false) {
+        $("#dialog2").dialog({
+            buttons: [
+                {
+                    text: "Take the risk",
+                    click: function () { // Er wordt geklikt/keuze gemaakt
+                        console.log("Take the risk");
+                        $(this).dialog("close"); // Pop up gaat weg
+                        // grabSword();
+                    }
+                },
+                {
+                    text: "I'll skip",
+                    click: function () { // Er wordt geklikt/keuze gemaakt
+                        console.log("I'll skip");
+                        $(this).dialog("close"); // Pop up gaat weg
+                        // skipSword();
+                    }
+                },
+            ],
+            open: disableBodyScrolling,
+            close: enableBodyScrolling
+        }).dialog("widget").find(".ui-dialog-title").hide();
+
+        dialog2HasBeenOpened = true;
+    } else if (heightOrWidth > scrollPositionDialog3 - 10 && heightOrWidth < scrollPositionDialog3 + 10 && dialog3HasBeenOpened === false) {
+        $("#dialog3").dialog({
+            buttons: [
+                {
+                    text: "Eat it!",
+                    click: function () { // Er wordt geklikt/keuze gemaakt
+                        console.log("Eat it!");
+                        $(this).dialog("close"); // Pop up gaat weg
+                        // eatMushroom();
+                    }
+                },
+                {
+                    text: "I hate mushrooms",
+                    click: function () { // Er wordt geklikt/keuze gemaakt
+                        console.log("I hate mushrooms");
+                        $(this).dialog("close"); // Pop up gaat weg
+                        // noEatMushroom();
+                    }
+                },
+            ],
+            open: disableBodyScrolling,
+            close: enableBodyScrolling
+        }).dialog("widget").find(".ui-dialog-title").hide();
+
+        dialog3HasBeenOpened = true;
+    } else if (heightOrWidth > scrollPositionDialog4 - 10 && heightOrWidth < scrollPositionDialog4 + 10 && dialog4HasBeenOpened === false) {
+        $("#dialog4").dialog({
+            buttons: [
+                {
+                    text: "Use sword",
+                    click: function () { // Er wordt geklikt/keuze gemaakt
+                        console.log("Use sword");
+                        $(this).dialog("close"); // Pop up gaat weg
+                        // attackSword();
+                    }
+                },
+                {
+                    text: "Slap him",
+                    click: function () { // Er wordt geklikt/keuze gemaakt
+                        console.log("Slap him");
+                        $(this).dialog("close"); // Pop up gaat weg
+                        // attackSlap();
+                    }
+                },
+                {
+                    text: "Scream at him",
+                    click: function () { // Er wordt geklikt/keuze gemaakt
+                        console.log("Scream at him");
+                        $(this).dialog("close"); // Pop up gaat weg
+                        // attackScream();
+                    }
+                },
+            ],
+            open: disableBodyScrolling,
+            close: enableBodyScrolling
+        }).dialog("widget").find(".ui-dialog-title").hide();
+
+        dialog4HasBeenOpened = true;
+    } else if (heightOrWidth > scrollPositionDialog5 - 10 && heightOrWidth < scrollPositionDialog5 + 10 && dialog5HasBeenOpened === false) {
+        $("#dialog5").dialog({
+            buttons: [
+                {
+                    text: "Return, duh",
+                    click: function () { // Er wordt geklikt/keuze gemaakt
+                        console.log("Return, duh");
+                        $(this).dialog("close"); // Pop up gaat weg
+                        // returnGold();
+                    }
+                },
+                {
+                    text: "I could use it...",
+                    click: function () { // Er wordt geklikt/keuze gemaakt
+                        console.log("I could use it...");
+                        $(this).dialog("close"); // Pop up gaat weg
+                        // keepGold();
+                    }
+                },
+            ],
+            open: disableBodyScrolling,
+            close: enableBodyScrolling
+        }).dialog("widget").find(".ui-dialog-title").hide();
+
+        dialog5HasBeenOpened = true;
     }
 }
 
-// } else if ($height === scrollPositionDialog2) {
-//     $("#dialog2").dialog("open");
-// } else if ($height === scrollPositionDialog3) {
-//     $("#dialog3").dialog("open");
-// } else if ($height === scrollPositionDialog4) {
-//     $("#dialog4").dialog("open");
-// } else if ($height === scrollPositionDialog5) {
-//     $("#dialog5").dialog("open");
-// }
-
 // Deze voor verticale scroll pagina's
-$(window).scroll(function() {
+$(window).scroll(function () {
     const currentHeight = $(window).scrollTop();
     checkScrollPosition(currentHeight);
 });
 
 // Deze voor horizontale scrollpagina
-$(window).scroll(function() {
+$(window).scroll(function () {
     const currentWidth = $(window).scrollLeft();
     checkScrollPosition(currentWidth);
 });
